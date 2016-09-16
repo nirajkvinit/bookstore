9a8859cb3d03f882ee34cf7043e88e7c7a558978
Nick Smolin <Nick27surgut@gmail.com>
2016-09-16 22:58:19 +0500

    workability template and registered store for admin module

--- main_site/store/views.py
+++ main_site/store/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
-
+# from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 from .models import Book
 
 
 def index(request):
-    latest_books_list = Book.objects.order_by('-approve_date')[:10]
-    context = {'latest_books_list': latest_books_list}
-    return render(request, 'store/index.html', context)
-# Create your views here.
+    latest_books_list = Book.objects.order_by('-approve_date')
+    template = loader.get_template('store/index.html')
+    context = RequestContext(request, {'books': latest_books_list})
+    return HttpResponse(template.render(context))
