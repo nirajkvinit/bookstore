cb4259e1da1737668198a2b95d42db545cc8d5b4
Nick Smolin <Nick27surgut@gmail.com>
2016-09-21 00:01:24 +0500

    new views for books,authors and genres

--- main_site/store/views.py
+++ main_site/store/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render_to_response
-from .models import Book
+from .models import Book, Genre, Author
 
 
 def index(request):
-    latest_books_list = Book.objects.order_by('-approve_date')
+    latest_books_list = Book.objects.order_by('-approve_date')[:10]
     return render_to_response('store/index.html', {'books': latest_books_list})
+
+
+def book_by_id(request, book_id):
+    current_book = Book.objects.get(id=book_id)
+    return render_to_response('store/book.html', {'book': current_book})
+
+
+def books_by_genre(request, genre_id):
+    current_genre = Genre.objects.get(id=genre_id)
+    books = Book.objects.filter(genre=genre_id)
+    return render_to_response('store/genre.html',
+                              {'books': books, 'genre': current_genre})
+
+
+def books_by_author(request, author_id):
+    current_author = Author.objects.get(id=author_id)
+    books = Book.objects.filter(author=author_id)
+    return render_to_response('store/author.html',
+                              {'books': books, 'author': current_author})
+
+
+def all_books(request):
+    all_books = Book.objects.order_by('-approve_date')
+    # реализовать пагинацию // realize pagination
+    return render_to_response('store/index.html', {'books': all_books})
