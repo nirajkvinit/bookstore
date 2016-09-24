d1d75b7767d7c713d6bdf3ecd074858599f204e9
Nick Smolin <Nick27surgut@gmail.com>
2016-09-25 00:00:35 +0500

    Registration form available

--- main_site/loginsys/urls.py
+++ main_site/loginsys/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^login/$', views.login),
     url(r'^logout/$', views.logout),
-    # url(r'^register/$', views.register),
+    url(r'^register/$', views.register),
 ]
--- main_site/loginsys/views.py
+++ main_site/loginsys/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response, redirect
 from django.contrib import auth
 from django.core.context_processors import csrf
-# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 def login(request):
@@ -26,14 +26,19 @@ def logout(request):
     return redirect('/')
 
 
-# def register(request):
-    # params = {}
-    # params.update(csrf(request))
-    # params['form'] = UserCreationForm()
-    # if request.POST:
-        # newuser_form = UserCreationForm(request.POST)
-        # newuser_form.save()
-        # newuser = auth.authenticate(username=newuser_form.cleaned_data['username'], password=newuser_form.cleaned_data['password'], email=newuser_form.cleaned_data['email'])
-        # auth.login(request, newuser)
-        # return redirect('/')
-    # return render_to_response('register.html', params)
+def register(request):
+    params = {}
+    params.update(csrf(request))
+    params['form'] = UserCreationForm()
+    if request.POST:
+        newuser_form = UserCreationForm(request.POST)
+        if newuser_form.is_valid():
+            newuser_form.save()
+            newuser = auth.authenticate(username=newuser_form.cleaned_data['username'],
+                                        password=newuser_form.cleaned_data['password'],
+                                        email=newuser_form.cleaned_data['email'])
+            auth.login(request, newuser)
+            return redirect('/')
+        else:
+            params['form'] = newuser_form
+    return render_to_response('loginsys/register.html', params)
