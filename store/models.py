6264874b169171aa7c223f12f6914fae88f885e1
Nick Smolin <Nick27surgut@gmail.com>
2017-03-13 17:07:46 +0300

    finish bc3

--- store/models.py
+++ store/models.py
@@ -7,13 +7,21 @@ import os
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+    def get_last_book(self):
+        return self.books.order_by('-approve_date')[0]
+
+    name = models.CharField(max_length=200)
+
 
 class Author(models.Model):
+
+    def __str__(self):
+        return self.name
+
     M = 'Male'
     F = 'Fem'
     GENDER_CHOICES = (
@@ -26,9 +34,6 @@ class Author(models.Model):
                               choices=GENDER_CHOICES,
                               default='Male')
 
-    def __str__(self):
-        return self.name
-
 
 class Book(models.Model):
 
@@ -36,6 +41,9 @@ class Book(models.Model):
         extension = filename.split('.')[-1]
         filename = "%s.%s" % (uuid4(), extension)
         return os.path.join("images/covers", filename)
+
+    def __str__(self):
+        return self.name
 
     name = models.CharField(max_length=200)
     coverpic = models.ImageField(upload_to=get_file_path,
@@ -45,8 +53,5 @@ class Book(models.Model):
                                     format='JPEG', options={'quality': 60})
     pub_date = models.DateField()
     approve_date = models.DateTimeField(auto_now_add=True)
-    genre = models.ForeignKey(Genre, on_delete=models.PROTECT)
+    genre = models.ForeignKey(Genre, related_name='books', on_delete=models.PROTECT)
     author = models.ForeignKey(Author, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.name
