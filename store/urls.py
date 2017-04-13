5c3743d7a2070b96626552489ae0586fd778804d
Nick Smolin <Nick27surgut@gmail.com>
2017-04-13 20:48:23 +0300

    slug references are work

--- store/urls.py
+++ store/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.all_books, name='store'),
     # book's page
     url(r'^book/(?P<book_id>[0-9]+)$', views.book_by_id, name='book_by_id'),
-    url(r'^book/(?P<slug>[0-9]+)$', views.book_by_id, name='book_by_slug'),
+    url(r'^book/(?P<slug>[a-z0-9/-]+)$', views.book_by_slug, name='book_by_slug'),
     url(r'^book/(?P<pk>[0-9]+)/delete_book/$', views.DeleteBook.as_view(), name='book_delete'),
     # add book to favorite
     url(r'^book/(?P<book_id>[0-9]+)/add_book_to_favorites/$', views.add_book_to_favorites, name='add_to_favorites'),
@@ -17,6 +17,7 @@ urlpatterns = [
     url(r'^genre/(?P<genre_id>[0-9]+)$', views.books_by_genre, name='books_by_genre'),
     # Author
     url(r'^author/(?P<author_id>[0-9]+)$', views.books_by_author, name='books_by_author'),
+    # url(r'^author/(?P<slug>[a-z0-9/-]+)$', views.books_by_author_slug, name='books_by_author_slug'),
     # Adding content
     url(r'^add_book/$', views.add_book, name='add_book'),
     url(r'^add_genre/$', views.add_genre, name='add_genre'),
